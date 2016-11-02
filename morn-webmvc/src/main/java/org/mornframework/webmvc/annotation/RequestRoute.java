41fc0412acce4fa250a4bee199151caa1c6e7fd1
Jeff.Li <lihuiace@163.com>
2016-11-02 16:14:27 +0800

    Copyright

--- morn-webmvc/src/main/java/org/mornframework/webmvc/annotation/RequestRoute.java
+++ morn-webmvc/src/main/java/org/mornframework/webmvc/annotation/RequestRoute.java
@@ -1,16 +1,18 @@
 /**
-* Copyright 2016-2026 the original author or authors.
-*
-* Welcome hot like Java Person,Contribution code for org.
-* This framework Has been published on Git
-*     https://github.com/chinalihui/morn-parent,
-*     https://git.oschina.net/osjeff/morn-parent
-* 
-* If there are problems or ideas can be submitted to the
-*     https://github.com/chinalihui/morn-parent/issues
-*
-*                                    Thanks.
-*/
+ * Copyright 2016- the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.mornframework.webmvc.annotation;
 
 import java.lang.annotation.Documented;
@@ -21,9 +23,10 @@ import java.lang.annotation.Target;
 
 import org.mornframework.webmvc.support.RequestMethod;
 
+
 /**
  * @author Jeff.Li
- * @date 2016年9月20日
+ * @date 2016年11月2日
  */
 @Target({ElementType.TYPE,ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
